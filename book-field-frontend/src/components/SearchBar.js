deade617e52645399f73239e5e71c195221992c3
loope3 <icewizardwoee@protonmail.com>
2024-02-19 20:44:17 -0500

    added functionality for search component params

--- book-field-frontend/src/components/SearchBar.js
+++ book-field-frontend/src/components/SearchBar.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './searchBar.css'
 
 function SearchBar() {
+
+    const [search, setSearch] = useState('');
+
+    function formatSearch(input) {
+
+        let result = input.split(' ').join('+');
+        setSearch(result);
+        console.log(result);
+        return result;
+    }
+
     return(
         <div className='search-container'>
             <form >
-                <input type='text' placeholder='Search books...' name='search' />
-                <button type='submit'><i class='fa fa-search'></i></button>
+                <input id='searchInput' type='text' placeholder='Search books...' name='search' />
+                <button type='submit' onClick={e=> {
+                    e.preventDefault();
+                    let searchValue = document.getElementById('searchInput');
+                    formatSearch(searchValue.value);
+                }}><i className='fa fa-search'></i></button>
             </form>
         </div>
     );
